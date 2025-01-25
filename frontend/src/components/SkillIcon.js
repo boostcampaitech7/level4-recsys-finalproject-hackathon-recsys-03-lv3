cd5006b23677dda891d0e15c9654d9b74f6e94e9
luck-kyv <kyk_0709@naver.com>
2025-01-26 01:09:32 +0900

    feat: FreelancerInfo.js 개발

--- frontend/src/components/SkillIcon.js
+++ frontend/src/components/SkillIcon.js
@@ -2,12 +2,19 @@ import React from "react";
 import "../style/SkillIcon.css";
 
 const getBackgroundColor = (score) => {
-  // 점수에 따라 색상 계산
-  const intensityRed = Math.floor(220 - score * 39.4);
-  const intensityGreen = Math.floor(220 - score * 8.4);
-  const intensityBlue = Math.floor(220 - score * 14.4);
+  // 점수에 따라 색상을 계산 (0 ~ 10 사이의 score)
+  const percentage = Math.min(Math.max(score / 10, 0), 1); // 0 ~ 1로 정규화
 
-  return `rgb(${intensityRed}, ${intensityGreen}, ${intensityBlue})`;
+  // 시작 색상 (#D9D9D9)와 끝 색상 (#17B294)을 RGB로 변환
+  const startColor = { r: 255, g: 235, b: 107 }; //rgb(255, 235, 107)
+  const endColor = { r: 23, g: 178, b: 148 }; // #17B294
+
+  // 색상 계산 (비례식)
+  const r = Math.round(startColor.r + (endColor.r - startColor.r) * percentage);
+  const g = Math.round(startColor.g + (endColor.g - startColor.g) * percentage);
+  const b = Math.round(startColor.b + (endColor.b - startColor.b) * percentage);
+
+  return `rgb(${r}, ${g}, ${b})`;
 };
 
 const SkillIcon = ({ text, score }) => {
@@ -18,7 +25,7 @@ const SkillIcon = ({ text, score }) => {
       }}
       className="badge skill-badge"
     >
-      {text}
+      {text} {score}
     </span>
   );
 };
