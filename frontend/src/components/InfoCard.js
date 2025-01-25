d879301c3260c27097a8c46cee89abe08f7b9d3f
luck-kyv <kyk_0709@naver.com>
2025-01-25 20:47:32 +0900

    feat: InfoCard.js, css파일 추가

---
+++ frontend/src/components/InfoCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../style/InfoCard.css";
+
+const InfoCard = ({ children, className = "" }) => {
+  return <div className={`info-card card${className}`}>{children}</div>;
+};
+
+export default InfoCard;
