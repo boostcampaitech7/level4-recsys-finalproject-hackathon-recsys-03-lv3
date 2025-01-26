c827d2ff024c2a8cb38b79f32f1964e043875262
0-virus <pyk09667@gmail.com>
2025-01-27 02:40:00 +0900

    fix: Layout.js 스타일 수정

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -11,9 +11,30 @@ const Layout = () => {
   };
 
   return (
-    <div id="wrapper" className="d-flex">
-      <div id="content-wrapper" className="d-flex flex-column">
-        <div id="content">
+    <div
+      id="wrapper"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh", // 화면 전체 높이를 보장
+      }}
+    >
+      <div
+        id="content-wrapper"
+        style={{
+          flex: 1, // 콘텐츠 영역이 남은 공간을 차지
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <div
+          id="content"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "95vh",
+          }}
+        >
           <Topbar toggleSidebar={toggleSidebar} />
           <div className="container-fluid">
             <Outlet />
