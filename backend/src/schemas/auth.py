ae2c6bea6166dc0210290ce77a778c455d634122
TaroSin <twokst@naver.com>
2025-02-03 18:29:05 +0900

    feat: auth.py 회원가입 api 추가

--- backend/src/schemas/auth.py
+++ backend/src/schemas/auth.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 
 class LoginRequest(BaseModel):
@@ -11,3 +12,25 @@ class LoginResponse(BaseModel):
     userId: int
     userName: str
     userType: int
+
+
+class FreelancerRegisterRequest(BaseModel):
+    freelancerName: str
+    email: str
+    password: str
+    workExp: int
+    price: int
+    workType: int
+    role: str
+    freelancerContent: Optional[str]
+    locationId: int
+    categoryList: List[int]
+    skillList: List[int]
+
+
+class CompanyRegisterRequest(BaseModel):
+    companyName: str
+    email: str
+    projectContent: Optional[str]
+    password: str
+    locationId: int
