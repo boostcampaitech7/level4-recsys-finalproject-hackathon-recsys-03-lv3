520debbdedee972a78bdc43a0d2830a126c089b5
TaroSin <twokst@naver.com>
2025-02-03 18:27:51 +0900

    feat: auth_route.py 회원가입 api 추가

--- backend/src/routes/auth_route.py
+++ backend/src/routes/auth_route.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from api.db import get_db
-from src.schemas.auth import LoginRequest, LoginResponse
+from src.schemas.auth import LoginRequest, LoginResponse, FreelancerRegisterRequest, CompanyRegisterRequest
 from src.services.auth_service import AuthService
 
 auth = APIRouter()
@@ -33,3 +33,35 @@ def logout_user():
     (클라이언트에서 JWT 토큰 삭제)
     """
     pass
+
+
+@auth.post("/register/freelancer")
+def register_freelancer(
+    freelancer_data: FreelancerRegisterRequest, db: Session = Depends(get_db)
+):
+    """
+    프리랜서 회원가입 API
+
+    프리랜서 계정을 생성하는 API로, 입력된 정보를 검증한 후 데이터베이스에 저장합니다.
+
+    Args:
+        freelancer_data (FreelancerRegisterRequest): 프리랜서 회원가입 요청 데이터
+        db (Session): SQLAlchemy 세션 객체
+    """
+    AuthService.register_freelancer(db, freelancer_data)
+    
+    
+@auth.post("/register/company")
+def register_company(
+    company_data: CompanyRegisterRequest, db: Session = Depends(get_db)
+):
+    """
+    기업 회원가입 API
+
+    기업 계정을 생성하는 API로, 입력된 정보를 검증한 후 데이터베이스에 저장합니다.
+
+    Args:
+        company_data (CompanyRegisterRequest): 기업 회원가입 요청 데이터
+        db (Session): SQLAlchemy 세션 객체
+    """
+    AuthService.register_company(db, company_data)
